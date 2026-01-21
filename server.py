49c729793bb45769aa598dac26a6b9cc56f16798
TastyInsam <iron3916@naver.com>
2026-01-21 22:26:47 +0900

    Update server.py

--- server.py
+++ server.py
@@ -40,7 +40,7 @@ async def chat_endpoint(request: ChatRequest):
     response = request.response
 
     # --- AI 처리 구간 ---
-    ai_response = await model_hub.run_model(response, user_name, user_id, target_persona, affinity, scene_num, client)
+    ai_response = model_hub.run_model(response, user_name, user_id, target_persona, affinity, scene_num, client)
 
     # ------------------------------------------
     return {
@@ -77,7 +77,7 @@ async def close_endpoint(request: ChatRequest):
     # embedding_model = model_manager.EMBEDDING_MODEL
     # client = rag_manager.database_check(collection_name = target_persona, embedding_model = embedding_model)
 
-    await model_hub.close_session(user_name = user_name, user_id = user_id, target_persona = target_persona, affinity = affinity, scene_num = scene_num, client = client)
+    model_hub.close_session(user_name = user_name, user_id = user_id, target_persona = target_persona, affinity = affinity, scene_num = scene_num, client = client)
 
     return {
         "response": "",
